f4d6f4bdc430aa6f67ee46c2161c685a716eea49
anushka <anushka.subedi@dnv.com>
2022-09-12 15:08:58 +0200

    added ImperfectAPI

---
+++ Imperfect.API/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Imperfect.API.Data;
+using Imperfect.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imperfect.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileController
+    {
+        private readonly ILogger<ProfileController> _logger;
+
+        public ProfileController(ILogger<ProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUsersProfile")]
+        public IEnumerable<Profile> Get()
+        {
+           return ProfileContext.Profiles;
+        }
+    }
+}
---
+++ Imperfect.API/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿namespace Imperfect.API.Models
+{
+    public class Profile
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public string ImageFile { get; set; }
+
+        
+
+    }
+}
