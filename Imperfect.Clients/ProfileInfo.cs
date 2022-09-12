0eba2e7838e718a6cb7cd47aee464bb7abba9657
Alethia Strande <alethiads@hotmail.com>
2022-09-12 10:54:19 +0200

    Adding profileinfo class

---
+++ Imperfect.Clients/ProfileInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Imperfect.Clients
+{
+    public class ProfileInfo
+    {
+        public String userName { get; set; }
+        public String mail { get; set; }
+        //should only be visible for yourself 
+        //This one should maybe be continously updated?
+        public List<String> followers { get; set; }
+        //This one should maybe be continously updated?
+        public List<Object> posts { get; set; }
+
+    }
+}
