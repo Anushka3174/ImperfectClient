c8a4ea8194a6efdd952698b08d8386c9365d015c
anushka <anushka.subedi@dnv.com>
2022-09-12 10:21:15 +0200

    this is my first commit

---
+++ Imperfect.Clients/Data/ProfileContext.cs
@@ -0,0 +1,88 @@
+﻿using Imperfect.Clients.Models;
+using System.Collections.Generic;
+
+
+
+
+namespace Imperfect.Clients.Data
+{
+
+        public static class ProfileContext
+        {
+        public static readonly List<Profile> Profiles = new List<Profile>
+        {
+            new Profile()
+            {
+                Name ="JohnDoe1",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile1.png",
+                DateOfBirth="01-08-1995",
+                Gender="Female"
+
+            },
+
+             new Profile()
+            {
+                Name ="JohnDoe2",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile2.png",
+                DateOfBirth="01-08-1995",
+                Gender="male"
+
+            },
+
+              new Profile()
+            {
+                Name ="JohnDoe3",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile1.png",
+                DateOfBirth="01-08-1995",
+                Gender="Female"
+
+            },
+
+               new Profile()
+            {
+                Name ="JohnDoe4",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile1.png",
+                DateOfBirth="01-08-1995",
+                Gender="Female"
+
+            },
+
+
+                new Profile()
+            {
+                Name ="JohnDoe5",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile1.png",
+                DateOfBirth="01-08-1995",
+                Gender="Female"
+
+            },
+
+
+                 new Profile()
+            {
+                Name ="JohnDoe6",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile1.png",
+                DateOfBirth="01-08-1995",
+                Gender="Female"
+
+            },
+
+                  new Profile()
+            {
+                Name ="JohnDoe7",
+                Description="lalalalallalaallala adshdsf rsfgyrfgbsfsdf iuseyrusiefhsdf fsoeiffydfhsd fs sefhusehfshf",
+                ImageFile="profile1.png",
+                DateOfBirth="01-08-1995",
+                Gender="Female"
+
+            },
+
+        };
+    }
+}
