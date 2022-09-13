94b8d6b6b651b980f7e93d5a9bb131b20b18ab9b
Alethia Strande <alethiads@hotmail.com>
2022-09-13 10:55:05 +0200

    Adding API templates

--- Imperfect.Clients/Controllers/HomeController.cs
+++ Imperfect.Clients/Controllers/HomeController.cs
@@ -30,5 +30,51 @@ namespace Imperfect.Clients.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //post/get friendship request 
+
+        //get person from search
+
+        //get persons feed 
+        [HttpGet(Name = "GetOnePersonFeed")]
+        public IActionResult GetOnePersonFeed()
+        {
+            return View();
+        }
+
+        //get feed 
+        [HttpGet(Name = "GetFeed")]
+        public IActionResult GetFeed()
+        {
+            return View();
+        }
+
+        //post picture/text
+        [HttpPost(Name = "PostContent")]
+        public IActionResult PostContent()
+        {
+            return View();
+        }
+        //get picture/text ???
+        [HttpPost(Name = "GetOneContent")]
+        public IActionResult GetOneContent()
+        {
+            return View();
+        }
+
+        //delete friend 
+        [HttpDelete(Name = "DeleteFriend")]
+        public IActionResult DeleteFriend()
+        {
+            return View();
+        }
+
+        //delete content 
+        [HttpDelete(Name = "DeleteContent")]
+        public IActionResult DeleteContent()
+        {
+            return View();
+        }
+
+        //like/comment friends posts
     }
 }
