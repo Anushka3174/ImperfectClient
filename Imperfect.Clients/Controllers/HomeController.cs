5204ba2c36e2105becb963d301064cee9a73c5fc
Alethia Strande <alethiads@hotmail.com>
2022-09-13 12:33:09 +0200

    Moved api calls

--- Imperfect.Clients/Controllers/HomeController.cs
+++ Imperfect.Clients/Controllers/HomeController.cs
@@ -30,56 +30,6 @@ namespace Imperfect.Clients.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        //post/get friendship request 
-
-        //get person from search
-
-        //get persons feed 
-        [HttpGet(Name = "GetOnePersonFeed")]
-        public IActionResult GetOnePersonFeed()
-        {
-            //sjekke om det er deg selv
-            //hente ut spesifikk bruker 
-            //sende med informasjon 
-                    // bilder/content 
-                    // annen informasjon
-            return View();
-        }
-
-        //get feed 
-        [HttpGet(Name = "GetFeed")]
-        public IActionResult GetFeed()
-        {
-            return View();
-        }
-
-        //post picture/text
-        [HttpPost(Name = "PostContent")]
-        public IActionResult PostContent()
-        {
-            return View();
-        }
-        //get picture/text ???
-        [HttpPost(Name = "GetOneContent")]
-        public IActionResult GetOneContent()
-        {
-            return View();
-        }
-
-        //delete friend 
-        [HttpDelete(Name = "DeleteFriend")]
-        public IActionResult DeleteFriend()
-        {
-            return View();
-        }
-
-        //delete content 
-        [HttpDelete(Name = "DeleteContent")]
-        public IActionResult DeleteContent()
-        {
-            return View();
-        }
-
-        //like/comment friends posts
+        
     }
 }
